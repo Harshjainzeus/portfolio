2a5708fa746d598c222ff97f108a8b9e7ef0ae23
zeus <zeusjaiun@zeuss-MacBook-Pro.local>
2025-09-11 14:11:15 +0530

    vite chnages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/harshjainzeus.github.io/',
+    base: '/portfolio/',
     plugins: [react()],
     build: {
         rollupOptions: {
